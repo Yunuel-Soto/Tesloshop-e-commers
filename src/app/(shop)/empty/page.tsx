3aa6e141cc3721ed837aeb57b391713b4e2f99f9
Yunuel-Soto <brianyunuelsotosanchezz@gmail.com>
2026-06-10 12:20:57 -0600

    empty page

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
 
 export default function() {
   return (
-    <div>
-        <h1>Empty page</h1>      
+    <div className="flex justify-center items-center h-200">
+      <IoCartOutline size={80} className="mx-5" />
+      
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">Tu carrito esta vacio</h1>
+        <Link className="text-blue-500 mt-1 text-xl" href={'/'}>Regresar</Link>
+      </div>
     </div>
   )
 }

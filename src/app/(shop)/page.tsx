0d43f032fbeabb47b974478d88d37d634348f855
Yunuel-Soto <brianyunuelsotosanchezz@gmail.com>
2026-06-15 13:20:32 -0600

    pagination complete

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,32 @@
-import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+interface Props {
+  searchParams: Promise<{
+    page?: string;
+  }>
+}
 
-  const products = initialData.products;
+export default async function Home({searchParams}: Props) {
+
+  const pagePromise = await searchParams;
+
+  const page = pagePromise.page ? parseInt(pagePromise.page) : 1;
+
+  const { products, currentPage, totalPages } = await getPaginatedProductsWithImages({
+    page
+  });
+
+  if (products.length == 0) {
+    redirect('/');
+  }
 
   return (
     <>
       <Title title="Tienda" subTilte="Todos los productos" className="mb-2" />
-      <ProductGrid products={products}/>
+      <ProductGrid products={products} />
+      <Pagination totalPages={totalPages}/>
     </>
   );
 }
